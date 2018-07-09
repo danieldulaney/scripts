354947e182b9fd97d85ad5e415587dd3a2cf9156
Daniel Dulaney <dulaney.daniel@gmail.com>
2018-07-09 04:13:02 +0000

    Added user to sudo group and install vim package

--- setup/arch-user.sh
+++ setup/arch-user.sh
@@ -3,9 +3,14 @@
 USERNAME=daniel
 
 # Install expected tools
-pacman -S git openssh sudo --noconfirm
+pacman -S vim git openssh sudo --noconfirm
 systemctl enable sshd.service
 systemctl start sshd.service
 
 # Create a new user
 useradd -m "$USERNAME"
+
+# Create sudo group if it doesn't exist, and add the new user to it
+$(getent group sudo) || groupadd sudo
+usermod -aG "$USERNAME"
+
