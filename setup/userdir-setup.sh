858fc2d3cf9cdabad33ecb518d3836a5f48f3aa5
Daniel Dulaney <dulaney.daniel@gmail.com>
2018-07-28 18:49:20 +0000

    Added .bashrc and refactored setup script

--- setup/userdir-setup.sh
+++ setup/userdir-setup.sh
@@ -1,11 +1,15 @@
 #! /usr/bin/env bash
 
 # Files that should be symlinked from the home directory
-to_link=(.bash_profile .gitconfig .tmux.conf .vimrc)
+to_link=(.bash_profile .bashrc .gitconfig .tmux.conf .vimrc)
 
 # Directory containing this source file
-dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+df_dir="$HOME/.dotfiles"
 
+# Git-clone dotfiles
+git clone git@github.com:danieldulaney/dotfiles.git $df_dir
+
+# Link to dotfiles
 for file in "${to_link[@]}" ; do
 
     # If a symlink exists, just delete it
@@ -15,10 +19,9 @@ for file in "${to_link[@]}" ; do
     # If any other file exists, rename it to .old
     elif [ -e "$HOME/$file" ] ; then
         mv "$HOME/$file" "$HOME/$file.old" 2> /dev/null
-
     fi
 
     # Create a new symlink
-    ln -s "$dir/$file" "$HOME/$file"
+    ln -s "$df_dir/$file" "$HOME/$file"
 done
 
