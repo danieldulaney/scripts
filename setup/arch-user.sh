046a790d355e452897421434272a22c2797d54d5
Daniel Dulaney <dulaney.daniel@gmail.com>
2018-07-28 17:47:51 +0000

    Added SSH forwarding to sshd config

--- setup/arch-user.sh
+++ setup/arch-user.sh
@@ -3,7 +3,7 @@
 USERNAME=daniel
 
 # Install expected tools
-pacman -S vim git openssh sudo --noconfirm
+pacman -S vim git openssh sudo tmux --noconfirm
 systemctl enable sshd.service
 systemctl start sshd.service
 
@@ -13,4 +13,7 @@ useradd -m "$USERNAME"
 # Create sudo group if it doesn't exist, and add the new user to it
 $(getent group sudo) || groupadd sudo
 usermod -aG "$USERNAME"
+
+# Allow SSH key forwarding
+echo 'AllowAgentForwarding yes' >> /etc/ssh/sshd_config
 
